8f18e963f6a51381f61ff06da28fe85909de41d4
kauaDBR <159450142+kauaDBR@users.noreply.github.com>
2024-04-05 08:53:59 -0300

    Add files via upload

---
+++ Sem Título1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    char c = 'a';
+    int x = 5;
+    float f = 5.25;
+    double d = 15.673;
+    double y = 5.0e10;
+    
+    int numeros[5] = {1, 2, 3, 4, 5};
+    for(size_t i = 0; i < 4; i++ )
+    {
+    	printf("%d \n", numeros[i]);
+	}
+	printf("o valor é c =%c \n", c);
+	printf("o valor é x =%d \n", x);
+	printf("o valor é f =%f \n", f);
+	printf("o valor é d =%f \n", d);
+	printf("o valor é y =%e \n", y);
+}
